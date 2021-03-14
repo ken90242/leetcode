5fee4a284f3193962d0cf7baf73cff0a78f73c16
ken90242 <ken90242@gmail.com>
2021-03-15 01:54:07 +0800

    Success

--- 57. Insert Interval.py
+++ 57. Insert Interval.py
@@ -9,19 +9,24 @@ class Solution:
         while i < len(intervals):
             s, e = intervals[i]
             
+            # latest interval has no intersection with new interval
             if newInterval[1] < s:
                 result.append(newInterval)
                 newInterval = None
                 break
             
+            # latest interval has no intersection with new interval
             if e < newInterval[0]:
                 result.append(intervals[i])
             else:
+                # merge the old and new interval
                 newInterval = [min(s, newInterval[0]), max(e, newInterval[1])]
             
             i += 1
         
+        # incase newInterval is the last
         if newInterval:
             result.append(newInterval)
 
+        # incase newInterval has been inserted and remaining intervals has no intersection with it
         return result + intervals[i:]
